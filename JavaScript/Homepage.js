d80612825e708096c312a79a9c4f7fd9fa208137
Can Bleul <Can.Bleul@nagarro.com>
2024-02-23 10:41:12 +0100

    Hompage Ver. 0.0.0.2

--- JavaScript/Homepage.js
+++ JavaScript/Homepage.js
@@ -6,33 +6,19 @@ const Admin = userData.admin;
 const JWTToken = userData.jwttoken;
 
 
-
-async function beitreten() {
+let beitreten_button = document.getElementById('beitreten_button')
+beitreten_button.addEventListener('click', () => {
     console.log('in der funktion')
+    var gruppenID = this.parentElement.getAttribute('data-gruppenid'); 
+
     const fetch = require('node-fetch');
 
-    // Constants
     const BASE_URL = "https://lbv.digital";
-    const OWNER_CREDENTIALS = {"email": "test", "password": "test"};
-    const GROUP_ID = 14; // Group ID for testing
-    
-    // Function to get JWT token
-//    async function getJwtToken(credentials) {
-//        const response = await fetch(`${BASE_URL}/login`, {
-//            method: 'POST',
-//            headers: {
-//                'Content-Type': 'application/json'
-//            },
-//            body: JSON.stringify(credentials)
-//        });
-//        const data = await response.json();
-//        return data.access_token;
-//    }
-    
+
     // Function to add a user to a group
-    async function addUserToGroup(token, userId, groupId) {
-        const data = {"userID": userId, "groupID": groupId};
-        const headers = {'Authorization': `Bearer ${token}`};
+    async function addUserToGroup(JWTToken, BenutzerID, gruppenID) {
+        const data = {"userID": BenutzerID, "groupID": gruppenID};
+        const headers = {'Authorization': `Bearer ${JWTToken}`};
         const response = await fetch(`${BASE_URL}/users_in_groups`, {
             method: 'POST',
             headers: {
@@ -44,23 +30,11 @@ async function beitreten() {
         return response;
     }
     
-    // Acquire JWT token
-    getJwtToken(OWNER_CREDENTIALS)
-        .then(token => {
-            // Assuming userID 1 for testing - replace with an actual userID from your system
-            const userId = 6;
-            // Add user to group
-            return addUserToGroup(token, userId, GROUP_ID);
-        })
-        .then(response => {
-            console.log(`Response Status Code: ${response.status}, Response: `, response.json());
-        })
-        .catch(error => console.error('Error:', error));
-}
+    addUserToGroup(JWTToken, BenutzerID, gruppenID).then(response => {
+        console.log(`Response Status Code: ${response.status}, Response: `, response.json());
+    })
+    .catch(error => console.error('Error:', error));
+    
+  
 
-let beitreten_button = document.getElementById('beitreten_button')
-beitreten_button.addEventListener('click', () => {
-    console.log('in der funktion')
-    let userdata = beitreten()
-    console.log(userdata)
 } )

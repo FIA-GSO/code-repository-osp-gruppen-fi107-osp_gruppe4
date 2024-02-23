e568584a6db8fbb75ee620aac265bc6604bc47d5
Can Bleul <Can.Bleul@nagarro.com>
2024-02-23 13:17:01 +0100

    Homepage Ver 0.0.0.3

--- JavaScript/Homepage.js
+++ JavaScript/Homepage.js
@@ -1,36 +1,94 @@
-const userData = JSON.parse(localStorage.getItem('currentUser'));
-const BenutzerID = userData.benutzerId;
-const Vorname = userData.Vorname;
-const EMail = userData.email;
-const Admin = userData.admin;
-const JWTToken = userData.jwttoken;
+document.addEventListener('DOMContentLoaded', () => {
+//const userData = JSON.parse(localStorage.getItem('currentUser'));
+const token = JSON.parse(localStorage.getItem('jwtToken'));
+//const BenutzerID = userData.benutzerId;
+//const Vorname = userData.Vorname;
+//const EMail = userData.email;
+//const Admin = userData.admin;
+const JWTToken = token.jwttoken;
+//const BASE_URL = "https://lbv.digital";
+const BASE_URL = "http://127.0.0.1:5000";
+const auth = {'Authorization': `Bearer ${JWTToken}`};
+
+// Alle Gruppen auslesen und auf der Webseite anzeigen
+
+const fetch = require('node-fetch');
+fetch(`${BASE_URL}/groups`,{
+    method: 'GET',
+    headers: {
+        'Content-Type': 'application/json',
+        auth 
+    },
+    body: JSON.stringify({
+        groupID: groupID,
+        ownerID: ownerID,
+        title: title,
+        description: description,
+        maxUsers: maxUsers
+    })
+    })
+    .then(response => {
+        if (response.ok) {
+            var alleGruppen = response.json();
+            alleGruppen.forEach(function(gruppe, index) {
+                var gruppenInfo = document.createElement("div");
+                gruppenInfo.classList.add("gruppen-info");
+            
+                var heading = document.createElement("h3");
+                heading.textContent = gruppe.name;
+            
+                var beschreibung = document.createElement("p");
+                beschreibung.textContent = gruppe.beschreibung;
+            
+                var nutzer = document.createElement("p");
+                nutzer.textContent = "Nutzer: " + gruppe.nutzer;
+            
+                // Erstellen des Lösch-Buttons für jede Gruppe
+                var deleteButton = document.createElement("button");
+                deleteButton.classList.add("delete-button");
+                deleteButton.textContent = "L";
+                deleteButton.onclick = function() {
+                  deleteGruppe(index);
+                };
+            
+                gruppenInfo.appendChild(heading);
+                gruppenInfo.appendChild(beschreibung);
+                gruppenInfo.appendChild(nutzer);
+                gruppenInfo.appendChild(deleteButton); // Hinzufügen des Lösch-Buttons zur Gruppe
+                gruppenContainer.appendChild(gruppenInfo);
+              });
+        //    return response.json();
+        } else {
+            throw new Error('Gruppen konnten nicht gelesen werden.');
+        }
+    })
 
 
+// In Gruppe beitreten
 let beitreten_button = document.getElementById('beitreten_button')
-beitreten_button.addEventListener('click', () => {
+beitreten_button.addEventListener('click', function (auth) {
     console.log('in der funktion')
     var gruppenID = this.parentElement.getAttribute('data-gruppenid'); 
 
     const fetch = require('node-fetch');
 
-    const BASE_URL = "https://lbv.digital";
+    
 
     // Function to add a user to a group
-    async function addUserToGroup(JWTToken, BenutzerID, gruppenID) {
-        const data = {"userID": BenutzerID, "groupID": gruppenID};
-        const headers = {'Authorization': `Bearer ${JWTToken}`};
+    async function addUserToGroup(auth, BenutzerID, gruppenID) {
+        const data = {"userID": BenutzerID, "groupID": gruppenID};     
         const response = await fetch(`${BASE_URL}/users_in_groups`, {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
-                headers
+                auth
             },
             body: JSON.stringify(data)
         });
         return response;
     }
     
-    addUserToGroup(JWTToken, BenutzerID, gruppenID).then(response => {
+    addUserToGroup(auth,JWTToken, BenutzerID, gruppenID).then(response => {
         console.log(`Response Status Code: ${response.status}, Response: `, response.json());
     })
     .catch(error => console.error('Error:', error));
@@ -38,3 +96,4 @@ beitreten_button.addEventListener('click', () => {
   
 
 } )
+})
